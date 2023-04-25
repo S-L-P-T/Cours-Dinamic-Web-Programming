7d0355dd8038d15cff1b880759970b3eab659d1b
S-L-P-T <81326242+S-L-P-T@users.noreply.github.com>
2023-04-25 18:19:47 +0200

    main

--- Projet/js/script.js
+++ Projet/js/script.js
@@ -1,92 +1,101 @@
+//Déclaration des selecteurs
 var vie = document.querySelector('#vie');
 var eau = document.querySelector('#eau');
 var o2 = document.querySelector('#o2');
-var time = document.querySelector('#time');
+var timecontainer = document.querySelectorAll('.time');
+var deplacer = document.querySelector('#deplacer');
+var mineraiscontainer = document.querySelectorAll('.minerais');
 
-var deplacer = document.querySelector('#deplacer')
+//Déclaration des minerais
+var ice = "../img/minerais/ice.webp";
+var cobalt = "../img/minerais/cobalt.webp";
+var iron = "../img/minerais/iron.webp";
+var magnesium = "../img/minerais/magnesium.webp";
+var silicon = "../img/minerais/silicon.webp";
+var titanium = "../img/minerais/titanium.webp";
 
-let taillevie = 100;
-setInterval(function reduireLargeurvie() {
-    taillevie--;
-    vie.style.width = taillevie + "%";
-},10000)
-document.querySelector('#deplacer').addEventListener("click", () => {
-    taillevie -= 1;
-    if (taillevie <= 0) {
-        vie.style.width = 0 + "%";
-        console.log('taille vie :'+taillevie);
-    } else {
-        vie.style.width = taillevie + "%";
-        console.log('taille vie :'+taillevie);
-    }
-    vie.style.width = taillevie + "%";
-    console.log('taille vie :'+taillevie);
-});
+//Déclaration des tableaux
+var time = [0,100,100,100];
+var minerai = ['ice', 'cobalt', 'iron', 'magnesium', 'silicon', 'titanium'];
+var minerais = [ice, cobalt, iron, magnesium, silicon, titanium];
+var materiaux = [];
 
-let tailleeau = 100;
-setInterval(function reduireLargeureau() {
-    tailleeau--;
-    eau.style.width = tailleeau + "%";
+//Fonctions temps
+setInterval(function() {
+    time[3]--;
+    time[0]++
+    timecontainer[3].style.width = time[3] + "%";
+    timecontainer[0].innerHTML = time[0];
+},1000)
+
+setInterval(function() {
+    time[2]--;
+    timecontainer[2].style.width = time[2] + "%";
 },5000)
-document.querySelector('#deplacer').addEventListener("click", () => {
-    tailleeau -= 2;
-    if (tailleeau <= 0) {
-        eau.style.width = 0 + "%";
-        console.log('taille eau :'+tailleeau);
-    } else {
-        eau.style.width = tailleeau + "%";
-        console.log('taille eau :'+tailleeau);
-    }
-});
 
-let tailleo2 = 100;
-setInterval(function reduireLargeuro2() {
-    tailleo2--;
-    o2.style.width = tailleo2 + "%";
-},1000)
-document.querySelector('#deplacer').addEventListener("click", () => {
-    tailleo2 -= 10;
-    if (tailleo2 <= 0) {
-        o2.style.width = 0 + "%";
-        console.log('taille o2 :'+tailleo2);
-    } else {
-        o2.style.width = tailleo2 + "%";
-        console.log('taille o2 :'+tailleo2);
-    }
-});
+setInterval(function() {
+    time[1]--;
+    timecontainer[1].style.width = time[1] + "%";
+},10000)
 
-let inc = 1
-setInterval(function reduireLargeuro2() {
-    let incr = inc++;
-    time.innerHTML = incr;
-},1000)
-
-
-
-
-const ice = "../img/minerais/ice.webp";
-const cobalt = "../img/minerais/Cobalt.webp";
-const iron = "../img/minerais/iron.webp";
-const magnesium = "../img/minerais/magnesium.webp";
-const silicon = "../img/minerais/silicon.webp";
-const titanium = "../img/minerais/titanium.webp";
-
-const minerais = [ice, cobalt, iron, magnesium, silicon, titanium];
-
-for (let i = 1; i <= 6; i++) {
+for (let i = 0; i <= minerais.length-1; i++) {
     let mineraisAleatoire = minerais[Math.floor(Math.random() * minerais.length)];
     let img = document.createElement("img");
     img.src = mineraisAleatoire;
+    img.id = i;
     img.width = "50";
-    document.querySelector(`#minerai${i}`).appendChild(img);
+    mineraiscontainer[i].appendChild(img);
 }
 
-document.querySelector('#deplacer').addEventListener("click", () => {
-    for (let i = 1; i <= 6; i++) {
+deplacer.addEventListener("click", () => {
+    time[1] = time[1]-1;
+    time[2] = time[2]-5;
+    time[3] = time[3]-10;
+    if (time[1] <= 0) {
+        time[1] = 0;
+        timecontainer[1].style.width = time[1] + "%";
+    } else {
+        timecontainer[1].style.width = time[1] + "%";
+    }
+    if (time[2] <= 0) {
+        time[2] = 0;
+        timecontainer[2].style.width = time[2] + "%";
+    } else {
+        timecontainer[2].style.width = time[2] + "%";
+    }
+    if (time[3] <= 0) {
+        time[3] = 0;
+        timecontainer[3].style.width = time[3] + "%";
+    } else {
+        timecontainer[3].style.width = time[3] + "%";
+    }
+    for (let i = 0; i <= minerais.length-1; i++) {
         let mineraisAleatoire = minerais[Math.floor(Math.random() * minerais.length)];
-        let img = document.querySelector(`#minerai${i}`).querySelector("img");
+        let img = mineraiscontainer[i].querySelector("img");
         img.src = mineraisAleatoire;
         img.width = "50";
     }
 });
 
+//Fonction pour rechercher les minerais
+function rechercheElement(src, minerai) {
+    for (let i = 0; i < minerai.length; i++) {
+        if (src.includes(minerai[i])) {
+            return minerai[i];
+      }
+    }
+    return null;
+}
+
+mineraiscontainer.forEach( input => input.addEventListener('click', e => {
+        let src = e.target.src;
+        if (src == undefined) {
+
+        } else {
+            materiaux.push(rechercheElement(src,minerai));
+            let id = e.target.id;
+            let img = mineraiscontainer[id].querySelector("img");
+            img.src = '';
+        }
+    })
+);
